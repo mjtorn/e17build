569f5e65af780bedbe3c28587a5f21ef0f7bb37c
Markus Törnqvist <mjt@fadconsulting.com>
2013-05-07 10:17:59 +0300

    Stricter link check

--- e17b/utils.py
+++ e17b/utils.py
@@ -4,7 +4,7 @@ def is_interesting(link):
     """Look at link and see if we want take note of it
     """
 
-    return '.tar' in link.attrib['href']
+    return '.tar' in link.attrib['href'] and link.attrib['href'].endswith('bz2')
 
 # EOF
 
