c1f261e9d7b07d261d2341cc53d6d45b2591d7b2
Markus Törnqvist <mjt@fadconsulting.com>
2013-05-07 10:22:59 +0300

    Split name extraction into own function

--- e17b/utils.py
+++ e17b/utils.py
@@ -6,15 +6,20 @@ def is_interesting(link):
 
     return '.tar' in link.attrib['href'] and link.attrib['href'].endswith('bz2')
 
+def name_from_link(link):
+    """Extract package name from link
+    """
+
+    url = link.attr('href')
+
+    return url.split('-', 1)[0]
+
 def pkg_name_sort(link1, link2):
     """Sort links by package name
     """
 
-    url1 = link1.attr('href')
-    url2 = link2.attr('href')
-
-    pkg_name1 = url1.split('-', 1)[0]
-    pkg_name2 = url2.split('-', 1)[0]
+    pkg_name1 = name_from_link(link1)
+    pkg_name2 = name_from_link(link2)
 
     return cmp(pkg_name1, pkg_name2)
 
