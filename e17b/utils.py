e45e8d5dd2ef06cc0ab8459e32417954c0968b38
Markus Törnqvist <mjt@fadconsulting.com>
2013-05-07 10:18:58 +0300

    Util to sort links by package name

--- e17b/utils.py
+++ e17b/utils.py
@@ -6,5 +6,17 @@ def is_interesting(link):
 
     return '.tar' in link.attrib['href'] and link.attrib['href'].endswith('bz2')
 
+def pkg_name_sort(link1, link2):
+    """Sort links by package name
+    """
+
+    url1 = link1.attr('href')
+    url2 = link2.attr('href')
+
+    pkg_name1 = url1.split('-', 1)[0]
+    pkg_name2 = url2.split('-', 1)[0]
+
+    return cmp(pkg_name1, pkg_name2)
+
 # EOF
 
