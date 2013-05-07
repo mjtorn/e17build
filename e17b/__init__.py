02047e1f3d5d375afe570883c64c5fa820ef297a
Markus Törnqvist <mjt@fadconsulting.com>
2013-05-07 14:44:53 +0300

    Builder code to the stage where it extracts files

--- e17b/__init__.py
+++ e17b/__init__.py
@@ -1,5 +1,7 @@
 # vim: encoding: utf-8
 
+from cStringIO import StringIO
+
 from pyquery import PyQuery as pq
 
 from . import utils
@@ -8,9 +10,17 @@ import itertools
 
 import os
 
+import tarfile
+
 RELEASES_URL = 'http://download.enlightenment.org/releases/'
 
 SRC_DL_DIR = os.path.join(os.path.expanduser('~'), 'src', 'e17')
+
+BUILD_ORDER = (
+    'eina', 'eet', 'evas', 'evas_generic_loaders', 'ecore', 'eio', 'embryo',
+    'edje', 'efreet', 'e_dbus', 'eeze', 'emotion', 'ethumb', 'elementary',
+    'enlightenment',
+)
 
 def get_package_dict(mirror):
     """Which packages are available in mirror?
@@ -54,6 +64,33 @@ def download_packages(dst, mirror, packages, force_download=False):
         else:
             utils.download(url, dst_file)
 
+def build_packages(packages, dst):
+    """Builder. Contains extraction too.
+    """
+
+    # Maybe this should be smarter too :D
+    for pkg in BUILD_ORDER:
+        assert packages.has_key(pkg), '%s missing' % pkg
+
+    required_set = set(BUILD_ORDER)
+    available_set = set(packages.keys())
+
+    # Wouldn't it be nice to build these in parallell?
+    extras = available_set.difference(required_set)
+    print 'Extra packages: %s' % ', '.join(extras)
+
+    # TODO: Maybe store only the latest version after download
+    for pkg in BUILD_ORDER:
+        pkg_file = packages[pkg][-1]
+        path = os.path.join(dst, pkg_file)
+        print path
+
+        with tarfile.open(path) as tar:
+            tar = tarfile.open(path)
+            dst_dir = utils.verify_clean_build_dir(dst, tar)
+            tar.extractall(path=dst, members=utils.safe_tar_files(tar, verbose=True))
+            tar.close()
+
 def main(args):
     """Tie all the pieces together to build e17
     """
@@ -61,6 +98,7 @@ def main(args):
     package_dict = get_package_dict(RELEASES_URL)
 
     download_packages(SRC_DL_DIR, RELEASES_URL, package_dict)
+    build_packages(package_dict, SRC_DL_DIR)
 
 # EOF
 
