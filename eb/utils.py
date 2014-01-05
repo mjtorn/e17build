203cd898433337af2bc1987fb3f91e0b1c89ffb6
Markus Törnqvist <mjt@fadconsulting.com>
2014-01-05 20:50:44 +0200

    enventor released only as gz

--- eb/utils.py
+++ eb/utils.py
@@ -19,7 +19,7 @@ def is_interesting(link):
     """Look at link and see if we want take note of it
     """
 
-    return '.tar' in link.attrib['href'] and link.attrib['href'].endswith('bz2')
+    return '.tar' in link.attrib['href'] and (link.attrib['href'].endswith('bz2') or link.attrib['href'].endswith('gz'))
 
 def name_from_link(link):
     """Extract package name from link
