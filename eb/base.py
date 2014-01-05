08ef334e7cf0f7f1e038f02ec7c81f4c3b0ee8af
Markus Törnqvist <mjt@fadconsulting.com>
2014-01-05 23:38:42 +0200

    How did this ever work? Does it work now? :D:D

--- eb/base.py
+++ eb/base.py
@@ -19,7 +19,8 @@ class EnlightenmentBuilder(object):
         """
 
         ## XXX: The later additions mostly untested.
-        autogen_cmd = ['./autogen.sh']
+        aclocal_cmd = ['aclocal']
+        autogen_cmd = ['./autogen.sh', '--prefix=%s' % dst_dir]
         conf_cmd = ['./configure', '--prefix=%s' % dst_dir]
         make_cmd = ['make', '-j%d' % thread_count]
         install_cmd = ['make', 'install']
@@ -28,13 +29,16 @@ class EnlightenmentBuilder(object):
         if os.path.exists(os.path.join(src_dir, 'setup.py')):
             utils.run(setup_py_cmd, src_dir)
         else:
-            if not os.path.exists(os.path.join(src_dir, 'configure')):
-                if os.path.exists(os.path.join(src_dir, 'autogen.sh')):
-                    utils.run(autogen_cmd, src_dir)
-                else:
-                    raise RuntimeError('Nothing found to do in %s' % src_dir)
+            if '/efl-' in src_dir:
+                utils.run(aclocal_cmd, src_dir)
 
-            utils.run(conf_cmd, src_dir)
+            if os.path.exists(os.path.join(src_dir, 'autogen.sh')):
+                utils.run(autogen_cmd, src_dir)
+            elif os.path.exists(os.path.join(src_dir, 'configure')):
+                utils.run(conf_cmd, src_dir)
+            else:
+                raise RuntimeError('Nothing found to do in %s' % src_dir)
+
             utils.run(make_cmd, src_dir)
             utils.run(install_cmd, src_dir)
 
