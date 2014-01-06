a4eb88b5bea1ff7a7c157556b6c3bc3fa5cb8a9d
Markus Törnqvist <mjt@fadconsulting.com>
2014-01-06 09:04:27 +0200

    Add edbus and fix etrophy/echievements

--- eb/e17b.py
+++ eb/e17b.py
@@ -18,9 +18,10 @@ import tarfile
 BUILD_ORDER = (
     # libs
     'rel/libs/efl', 'rel/libs/evas_generic_loaders', 'rel/libs/emotion_generic_players', 'rel/libs/elementary',
+    'e_dbus',
     # apps
     'enlightenment', 'rel/apps/econnman', 'rel/apps/enventor', 'rel/apps/terminology',
-    'echievements', 'etrophy', 'elemines',
+    'etrophy', 'echievements', 'elemines',
     # bindings
     'rel/bindings/python',
 )
