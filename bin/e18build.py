22f95f9a1ce0505ad67beb8345d0d2f5086192c0
Markus Törnqvist <mjt@fadconsulting.com>
2014-01-06 09:58:40 +0200

    Proper import of utils

--- bin/e18build.py
+++ bin/e18build.py
@@ -5,13 +5,13 @@ import os
 
 import sys
 
-from eb import e17b
+from eb import e18b
 from eb import utils
 
 DEFAULTS = {
-    'srcdir': os.path.join(os.path.expanduser('~'), 'src', 'e17'),
-    'instpath': os.path.join(os.path.expanduser('~'), 'e17'),
-    'thread_count': utils.get_thread_count()
+    'srcdir': os.path.join(os.path.expanduser('~'), 'src', 'e18'),
+    'instpath': os.path.join(os.path.expanduser('~'), 'e18'),
+    'thread_count': e18b.utils.get_thread_count()
 }
 
 # Why do I have to assign this manually?
@@ -19,10 +19,10 @@ __doc__ = """
 E17 builder
 
 Usage:
-  e17build.py [--mirror=<url>] [--srcpath=<path>] [--instpath=<path>] [--thread-count=<n>] [--no-python] [--no-clean] [--rebuild]
+  e18build.py [--mirror=<url>] [--srcpath=<path>] [--instpath=<path>] [--thread-count=<n>] [--no-python] [--no-clean] [--rebuild]
 
 Options:
-  -m <url>, --mirror=<url>      Where to download from [default: http://download.enlightenment.org/]
+  -m <url>, --mirror=<url>      Where to download from [default: http://download.enlightenment.org/releases/]
   -s <path>, --srcpath=<path>   Where to download to [default: %(srcdir)s]
   -i <path>, --instpath=<path>  Where to install to [default: %(instpath)s]
   -t <n>, --thread-count=<n>    How many threads to (try to) use in compiling [default: %(thread_count)d]
@@ -35,7 +35,7 @@ from docopt import docopt
 
 if __name__ == '__main__':
     args = docopt(__doc__)
-    builder = e17b.E17Builder(args)
+    builder = e18b.E18Builder(args)
     sys.exit(builder.main(args))
 
 # EOF
