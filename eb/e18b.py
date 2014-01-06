8d32f77a6bca75514332c9411c189f309fd3c8e5
Markus Törnqvist <mjt@fadconsulting.com>
2014-01-06 09:55:32 +0200

    Some class

--- eb/e18b.py
+++ eb/e18b.py
@@ -1,5 +1,7 @@
 # vim: encoding: utf-8
 
+from .base import EnlightenmentBuilder
+
 BUILD_ORDER = (
     # libs
     'rel/libs/efl', 'rel/libs/evas_generic_loaders', 'rel/libs/emotion_generic_players', 'rel/libs/elementary',
@@ -10,6 +12,17 @@ BUILD_ORDER = (
     # bindings
     'rel/bindings/python',
 )
+
+class E18Builder(EnlightenmentBuilder):
+    """Build e18
+    """
+
+    def __init__(self, args, build_order=BUILD_ORDER):
+        """Set kwargs from this file
+        """
+
+        self.args = args
+        self.build_order = BUILD_ORDER
 
 # EOF
 
