6129317cdb65ab874f88aed81f538660b818a591
Markus Törnqvist <mjt@fadconsulting.com>
2014-01-06 09:51:05 +0200

    Adapt e18 builder

--- eb/e18b.py
+++ eb/e18b.py
@@ -1,4 +1,13 @@
 # vim: encoding: utf-8
 
+BUILD_ORDER = (
+    # libs
+    'efl', 'elementary', 'emotion_generic_players', 'evas_generic_loaders', 'webkit-efl',
+    # apps
+    'enlightenment', 'econnman', 'enventor', 'terminology',
+    # bindings
+    'python-efl',
+)
+
 # EOF
 
